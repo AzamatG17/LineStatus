48c73cd769e63b1c93ff637fed90329ba5c154d4
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-05-05 10:41:40 +0500

    minor update

---
+++ LineStatus.Api/LineStatus.Api/Program.cs
@@ -0,0 +1,42 @@
+using LineStatus.Api.Interfaces;
+using LineStatus.Api.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+using System.Reflection;
+using Carter;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<IApplicationDbContext, ApplicationDbContext>((serviceProvider, options) =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
+
+builder.Services.AddCarter();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapScalarApiReference();
+    app.MapOpenApi();
+
+    app.MapGet("/", () => Results.Redirect("/scalar/v1"));
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapCarter();
+
+app.Run();
